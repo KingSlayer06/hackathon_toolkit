d0da028ea96fe7ef8cea8faf79c1888867ee501b
KingSlayer06 <sherkarhimanshu00@gmail.com>
2026-04-25 10:29:08 +0200

    KMP removed

--- composeApp/src/androidMain/kotlin/com/kingslayer06/vox_bunq_hackathon_70/Platform.android.kt
+++
@@ -1,9 +0,0 @@
-package com.kingslayer06.vox_bunq_hackathon_70
-
-import android.os.Build
-
-class AndroidPlatform : Platform {
-    override val name: String = "Android ${Build.VERSION.SDK_INT}"
-}
-
-actual fun getPlatform(): Platform = AndroidPlatform()
--- composeApp/src/commonMain/kotlin/com/kingslayer06/vox_bunq_hackathon_70/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.kingslayer06.vox_bunq_hackathon_70
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
